f8907b02d0513e11506147f29ae72a143133adeb
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-06-09 17:41:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ window.onload = function(){
    document.getElementById('particleFrame').src = 'https://editor.p5js.org/anubhav0708/embed/jfdSCQNU2';
  },10000);
 };
+window.onscroll = function() {myFunction()};
+var div1=document.getElementByID("div1")
+var div1y = navlist.offsetTop;
+var i = 0;
+var txt = '<h1 style="font-size:80px;">Hi There!</h1><br>welcome to my site I am anubhav a developer by hobby I started developing when I was at 6th standard in 2019';
+var speed = 50;
+
+		/* Function to stick the nav bar */
+		function myFunction() {
+			if (  div1y>(window.pageYOffset-80)) {
+			typeWriter();	
+			}
+			else {
+				
+			}
+  function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("div1").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+		}
