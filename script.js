7eca35dc107c491b939fda3d75adc4e30d531ed0
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-06-10 16:42:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = function(){
  },10000);
 };
 window.onscroll = function() {myFunction()};
-var div1v=document.getElementByID("div1")
+var div1v=getElementById('div1');
 var div1y = div1v.offsetTop;
 var i = 0;
 var txt = 'welcome to my site I am anubhav a developer by hobby I started developing when I was at 6th standard in 2019';
@@ -12,7 +12,7 @@ var speed = 50;
 
 		/* Function to stick the nav bar */
 		function myFunction() {
-			if (document.body.scrollTop> 300) {
+			if ((document.body.scrollTop+window.innerHeight)>div1y) {
 			typeWriter();	
 			}
 			else {
