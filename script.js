2559cfce1f713dce4cbfd4476e4c29670542ceff
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-08-13 14:56:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,71 @@
 window.onload = function(){
- setTimeout(function(){
-   document.getElementById('particleFrame').src = 'https://editor.p5js.org/anubhav0708/embed/jfdSCQNU2';
- },10000);
-};
-window.onscroll = function() {myFunction()};
-var div1v=document.getElementById('div1');
-var div1y = div1v.offsetTop;
-var i = 0;
-var txt = 'welcome to my site I am anubhav a developer by hobby I started developing when I was at 6th standard in 2019';
-var speed = 50;
-
-		/* Function to stick the nav bar */
-		function myFunction() {
-			if ((document.body.scrollTop+window.innerHeight)>div1y) {
-			typeWriter();	
-			}
-			else {
-				
-			}
-  function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("div1").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+    myFunction();
+    setTimeout(function(){
+      document.getElementById('particleFrame').src = 'https://editor.p5js.org/anubhav0708/embed/jfdSCQNU2';
+    },1000);
+    
+   };
+   window.addEventListener("click", function(){
+    window.navigator.vibrate(50);
+    var audiov = document.getElementById("myAudio"); 
+    audiov.play();
+  });
+   var div1v=document.getElementById('div1');
+   var div1y = div1v.offsetTop;
+   var chatbotv=document.getElementById('div1');
+   var chatboty = div1v.offsetTop;
+   var i = 0;
+   var txt = ' to my site I am Anubhav a hobbyist developer';
+   var speed = 50;
+   
+           /* Function to stick the nav bar */
+function myFunction() {
+div1y = div1v.offsetTop; 
+if ((window.pageYOffset+window.innerHeight)>div1y) {
+typeWriter();
 }
-		}
+else {
+
+ }
+ setTimeout(function(){
+    myFunction();
+  },2000);
+ }
+     function typeWriter() {
+     if (i < txt.length) {
+       document.getElementById("div1").innerHTML += txt.charAt(i);
+      i++;
+       setTimeout(typeWriter, speed);
+     }
+   }
+   document.documentElement.onclick = function() {
+    navigator.vibrate(100);
+    document.getElementById("myAudio").play();
+   };
+   document.getElementById('particleFrame').onclick = function() {
+    navigator.vibrate(100);
+    document.getElementById("myAudio").play();
+   };
+   function checkBrowser() {
+          
+    // Get the user-agent string
+    let userAgentString = 
+        navigator.userAgent;
+  
+    // Detect Chrome
+       if( userAgentString.indexOf("Chrome") > -1)
+       {
+
+       }// Detect Firefox
+       else if( userAgentString.indexOf("Firefox") > -1)
+       {
+
+       } // Detect Safari
+       else if (userAgentString.indexOf("Safari") > -1)
+       {
+
+       }
+       else {
+
+       }
+}
