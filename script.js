65225d32fe461fe9f7ed0f8d7cb4c5dcaae3bd25
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-08-16 11:23:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     myFunction();
     setTimeout(function(){
-      document.getElementById('particleFrame').src = 'https://editor.p5js.org/anubhav0708/embed/jfdSCQNU2';
+     document.getElementById('particleFrame').src = 'https://editor.p5js.org/anubhav0708/embed/jfdSCQNU2';
     document.getElementById('flag').src = 'https://anubhavsingh0708.github.io/AnubhavSingh0708/3d/';
     },10000);
    };
