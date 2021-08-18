00e17a93fb9e6923247e6231f7748d7da6b49067
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-08-18 20:47:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ window.onload = function(){
         var head = document.getElementsByTagName('HEAD')[0].innerHTML;
         document.getElementsByTagName('HEAD')[0].innerHTML=head+='<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet">'
         document.body.style.style.fontFamily="ZCOOL QingKe HuangYou', cursive";
-    },10000);
+    },5000);
    };
    window.addEventListener("click", function(){
     window.navigator.vibrate(50);
